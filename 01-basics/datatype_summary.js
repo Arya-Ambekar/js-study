b0b6ddae48a1f4a3f3a01df325d2cf17880ec687
Arya Ambekar <142108936+Arya-Ambekar@users.noreply.github.com>
2024-05-07 15:04:15 +0000

    datatypes summary

---
+++ 01-basics/datatype_summary.js
@@ -0,0 +1,48 @@
+    //  Primitive
+
+//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+const score = 100
+//console.log(typeof score);
+
+const scoreValue = 100.3
+//console.log(typeof scoreValue);
+
+const isLoggedIn = false
+//console.log(typeof isLoggedIn);
+
+const outsideTemp = null              
+// console.log(typeof outsideTemp);      // o/p --> object
+
+let userEmail;
+// console.log(typeof userEmail);           // o/p --> undefined
+
+const id = Symbol('123')
+// console.log(typeof id); 
+
+const anotherId = Symbol('123')
+// console.log(typeof anotherId); 
+
+console.log(id === anotherId);
+
+const bigNumber = 3456543576654356754n
+// console.log(typeof bigNumber); 
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof myFunction);
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
