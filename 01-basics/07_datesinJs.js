d0985c8c4155a1da08dd932253235a553738c32e
Arya Ambekar <142108936+Arya-Ambekar@users.noreply.github.com>
2024-05-09 08:23:35 +0000

    Dates

---
+++ 01-basics/07_datesinJs.js
@@ -0,0 +1,11 @@
+let myDate = new Date()
+
+// console.log(myDate);
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toLocaleTimeString());
+// console.log(myDate.toLocaleString());
+
+let myCreatedDate = new Date(10, 5, 2023);
+console.log(myCreatedDate);
+
